577619c7d4d1b843fe893a7c930fd613b67a3740
santhoshtn <santhoshtn1997@gmail.com>
2019-02-25 17:37:13 +0530

    djbfj

--- login-service/src/main/java/com/stackroute/LoginServiceApplication.java
+++ login-service/src/main/java/com/stackroute/LoginServiceApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class LoginServiceApplication  {
 
 	public static void main(String[] args) {
